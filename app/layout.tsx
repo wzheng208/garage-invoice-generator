4c62b2c22694aa01d131f5a0efc3555a227741dd
Wendy Zheng <wzheng208@gmail.com>
2026-04-22 12:04:38 -0400

    enable vercel analytic + redeploying

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
     >
       <body className='min-h-screen bg-[#f5f5f5] text-[#171a20]'>
         {children}
+        {/* Enabling Vercel Analytics to track usage and improve the app based on user interactions. */}
         <Analytics />
       </body>
     </html>
